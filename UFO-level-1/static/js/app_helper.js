ecdfee24156f51b44e830c2984653db4e2898deb
John Cass <cass.john@icloud.com>
2021-07-27 17:32:20 -0500

    used Erins starter file to build UFO 1 page

--- UFO-level-1/static/js/app_helper.js
+++ UFO-level-1/static/js/app_helper.js
@@ -5,50 +5,67 @@
 // from data.js
 const tableData = data;
 
-// get table references where table will be inserted
-var tbody = d3.select("tbody");
+//reference table body
+let tbody = d3.select("tbody");
 
+// Console.log the ufo data from data.js
 console.log(tableData);
 
-// When the page loads, it needs to display the table
-// But if the table reloads then you may need to ensure the 
-// previous output is cleared/overwritten from scratch 
-
-// Think of the class activities for generating tables
 function buildTable(tableData) {
-  tableData.forEach((ufoReport) => {
-      let row = tbody.append("tr");
-      Object.entries(ufoReport).forEach(([key, value]) => {
-        let cell = row.append("td");
-        cell.text(value);
+    //enter data into table
+    tableData.forEach((ufoReport) => {
+        let row = tbody.append("tr");
+        Object.entries(ufoReport).forEach(([key, value]) => {
+          let cell = row.append("td");
+          cell.text(value);
+        });
       });
-    });
-};
-
-function handleClick() {
-
-  // Grab the datetime value from the filter
+  };
   
-
-  // grab all the table data and set to filteredData
+  function handleClick() {
+    d3.event.preventDefault();
+    // Grab the datetime value from the filter
+    let inputElement = d3.select("#datetime");
+    let inputValue = inputElement.property("value");
+    
+    console.log(inputValue);
   
-  // Check to see if a date was entered and filter the
-  // data using that date.
+    // grab all the table data and set to filteredData
+    let filteredData = tableData.filter(d => d.datetime === inputValue);
+    console.log(filteredData);
 
+    // Check to see if a date was entered and filter the
+    // data using that date.
+    let dates = tableData.map(tableData => tableData.datetime);
+    if (dates.includes(inputValue)) {
+        console.log(filteredData);
+        console.log("if statement works");
+        //clears error message if exists
+        d3.select(".filter_error").text("");
+        //clears any previous data in table
+        tbody.html("");
+    }
+    else if (inputValue === ""){
+      //clears table if exists
+      tbody.html(""); 
+      //clears error message if exists
+        d3.select(".filter_error").text("");
+        //loads original data (no filter) into table
+        buildTable(tableData);
+    }
+    else if (!dates.includes(inputValue)){
+        tbody.html("");
+        d3.select(".filter_error").text("That date doesn't exist in our data. Enter a new date");
+        console.log("That date does not exist");
+    }
 
+    // Rebuild the table using the filtered data
+    buildTable(filteredData);
+  };
   
-
+  // Attach an event to listen for the form button that calls handleClick on a click of the filter button
+  let button = d3.select("#filter-btn");
+  button.on("click", handleClick);
   
-
-  // Rebuild the table using the filtered data
-  // @NOTE: If no date was entered, then filteredData will
-  // just be the original tableData.
-  // take your filtered data and put it into the buildTable to rebuild the table with the filtered data
-  buildTable(filteredData);
-}
-
-// Attach an event to listen for the form button that calls handleClick on a click of the filter button
-
-
-// Build the table when the page loads
-buildTable(tableData);
+  // Build the table when the page loads
+  buildTable(tableData);
